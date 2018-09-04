ff8e86cfa0b13988dbbb2a4d936ed8f7c9a260f3
cthompson7 <37081014+cthompson7@users.noreply.github.com>
2018-09-04 18:13:21 -0400

    Completed In-Class Activity, Session 02

--- Session01/calc.py
+++ Session01/calc.py
@@ -1,34 +1,104 @@
 # Exercise 2
 #Rewrite calc.py to solve the following questions.
 
-# 1.) How many seconds are there in 42 minutes 42 seconds?
+# # 1.) How many seconds are there in 42 minutes 42 seconds?
+#
+# totalSeconds = str((42*60) + 42)
+#
+# print("There are " + totalSeconds + " seconds.")
+#
+# # 2.) How many miles are there in 10 kilometers? Hint: there are 1.61 kilometers in a mile.
+#
+# totalMiles = str(10 / 1.61)
+#
+# print("There are " + totalMiles + " miles in 10 kilometers.")
+#
+# # 3.) If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace (time per mile in minutes and seconds)? What is your average speed in miles per hour?
+#
+# # Average Pace calculation
+#
+# averagePaceInSeconds = ((42*60) + 42) / (10/1.61)
+#
+# averagePaceInMinutes = averagePaceInSeconds / 60
+#
+# avgPaceMinutesOnly = str(int(averagePaceInMinutes))
+#
+# avgPaceSecondsOnly = str(round(60 - ((round(averagePaceInMinutes) - averagePaceInMinutes) * 60)))
+#
+# print("Your average pace per mile is " + avgPaceMinutesOnly + " minutes and " + avgPaceSecondsOnly + " seconds.")
+#
+# # Average Speed Calculation
+#
+# milesPerHour = str((3600/((42*60) + 42)) * (10 / 1.61))
+#
+# print("Your average speed in miles per hour is " + milesPerHour + ".")
 
-totalSeconds = str((42*60) + 42)
 
-print("There are " + totalSeconds + " seconds.")
+# Exercise 1 for Session 02
 
-# 2.) How many miles are there in 10 kilometers? Hint: there are 1.61 kilometers in a mile.
+# 1.) The volume of a sphere with radius r is
+    #(4/3)πr3.
 
-totalMiles = str(10 / 1.61)
+    #What is the volume of a sphere with radius 5?
 
-print("There are " + totalMiles + " miles in 10 kilometers.")
+import math
+radius = 5
+volume = (4/3)*(math.pi)*(radius**3)
 
-# 3.) If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace (time per mile in minutes and seconds)? What is your average speed in miles per hour?
+print('The volume of a sphere with radius 5 is', volume)
 
-# Average Pace calculation
+# 2.) Suppose the cover price of a book is $24.95, but bookstores get a 40\% discount.
+    #Shipping costs $3 for the first copy and 75 cents for each additional copy.
+    #What is the total wholesale cost for 60 copies?
 
-averagePaceInSeconds = ((42*60) + 42) / (10/1.61)
+numberOfBooks = 60
+wholesaleCost = (24.95-(24.95*.4))*numberOfBooks + (numberOfBooks-1 *.75)
 
-averagePaceInMinutes = averagePaceInSeconds / 60
+def wholesaleTotal(wholesaleCost):
+    if numberOfBooks >= 1:
+        return wholesaleCost + 3.00
+    else:
+        return wholesaleCost
 
-avgPaceMinutesOnly = str(int(averagePaceInMinutes))
+print("The total wholesale cost for 60 copies is: ${:.2f}".format(wholesaleTotal(wholesaleCost)))
 
-avgPaceSecondsOnly = str(round(60 - ((round(averagePaceInMinutes) - averagePaceInMinutes) * 60)))
+# 3.) If I leave my house at 6:52 am and run 1 mile at an easy pace (8:15 per mile),
+    #then 3 miles at tempo (7:12 per mile) and 1 mile at easy pace again,
+    #what time do I get home for breakfast?
 
-print("Your average pace per mile is " + avgPaceMinutesOnly + " minutes and " + avgPaceSecondsOnly + " seconds.")
+easy_pace = 8*60 + 15
+tempo_pace = 7*60 + 12
+total_time = 2*easy_pace + 3*tempo_pace
+timeInMinutes = round(total_time/60)
+timeInSecs = ((total_time/60 - round(total_time/60)) *60)
+startHour = 6
+startMinute = 52
 
-# Average Speed Calculation
+def minutes(timeInMinutes, startMinute):
+    endMinute = 0
+    if startMinute + timeInMinutes > 60:
+        endMinute = startMinute + timeInMinutes - 60
+        return endMinute
+    else:
+        endMinute = startMinute + timeInMinutes
+        return endMinute
 
-milesPerHour = str((3600/((42*60) + 42)) * (10 / 1.61))
+def hours(startHour):
+    additionalhours = 0
+    endHour = 0
+    if startMinute + timeInMinutes > 60:
+        additionalhours = additionalhours + 1
+    endHour = additionalhours + startHour
+    return endHour
 
-print("Your average speed in miles per hour is " + milesPerHour + ".")
+print("You get home for breakfast at {}:{}am.".format(hours(startHour),minutes(timeInMinutes, startMinute)))
+
+# 4.) If my average grade rises from 82 to 89. What is the percentage of the increase?
+    #Format the result as 'xx.x%'. Keep one figure after decimal point.
+
+changeInGrade = (89-82)
+percentIncrease = (changeInGrade / 82) * 100
+
+print("The percentage of the increase is {:.1f}%.".format(percentIncrease))
+
+
---
+++ Session02/demo-02.py
@@ -0,0 +1,69 @@
+# print('Hello, world!')
+
+# print('Hey Jude, Don\'t make it bad')
+
+# print('The sum is', 2 + 2 + 2 + 2)
+
+# name = input()
+#
+# print('Hello, ', name)
+
+# print('Please enter your name:')
+# name = input()
+# print('Hello, ', name)
+
+# name = input('Please enter your name: ')
+# print("Hello, ", name)
+
+# message = "I learned something cool today"
+# n = 100
+# pi = 3.14
+#
+# print(n)
+
+# a = 123
+# print(a)
+# a = "ABC"
+# print(a)
+
+# x = 10
+# x = x + 2
+# print(x)
+
+# a = 'ABC'
+# b = a
+# a = 'XYZ'
+# print(b)
+# print(a)
+#
+# '2' + '1'
+
+# first_name = 'Christian'
+# last_name = 'Thompson'
+# name = first_name + " " + last_name
+# print(name)
+#
+# name = '%s %s' %(first_name, last_name)
+# print(name)
+
+# first_name = input("Please enter your first name: ")
+# last_name = input("Please enter your last name: ")
+#
+# print("Hello, ", first_name + " " + last_name)
+
+# print("Hello, {}".format("world"))
+#
+# name = 'python'
+# print('Hello, {}'.format(name))
+#
+# print('Congratulations, {:s}, you won {:d}th Academy award.'.format(name, 90))
+
+# print('Pi equals {:010.2f}'.format(3.14159265))
+
+# print('Growth rate: {:.2f}%'.format(2.5))
+#
+# print('Coordinates of Babson: {lat}, {lon}'.format(lat='42.30N', lon='71.27W'))
+
+# print('Coordinates of Babson: %s, %s'% ('42.30N', '71.27W'))
+
+
